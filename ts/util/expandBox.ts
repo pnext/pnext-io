724176c052b685d987dd194bdb9305c9a1c46a4e
Martin Heidegger <martin.heidegger@gmail.com>
2018-07-17 10:57:33 +0900

    Making sure that expandBox returns THREE objects.

--- ts/util/expandBox.ts
+++ ts/util/expandBox.ts
@@ -1,16 +1,17 @@
 import IBox3 from '../api/IBox3'
+import { Box3, Vector3 } from 'three'
 
-export default function expandBox (boxA: IBox3, boxB: IBox3): IBox3 {
-  return {
-    min: {
-      x: Math.min(boxA.min.x, boxB.min.x),
-      y: Math.min(boxA.min.y, boxB.min.y),
-      z: Math.min(boxA.min.z, boxB.min.z)
-    },
-    max: {
-      x: Math.max(boxA.max.x, boxB.max.x),
-      y: Math.max(boxA.max.y, boxB.max.y),
-      z: Math.max(boxA.max.z, boxB.max.z)
-    }
-  }
+export default function expandBox (boxA: IBox3, boxB: IBox3): Box3 {
+  return new Box3(
+    new Vector3(
+      Math.min(boxA.min.x, boxB.min.x),
+      Math.min(boxA.min.y, boxB.min.y),
+      Math.min(boxA.min.z, boxB.min.z)
+    ),
+    new Vector3(
+      Math.max(boxA.max.x, boxB.max.x),
+      Math.max(boxA.max.y, boxB.max.y),
+      Math.max(boxA.max.z, boxB.max.z)
+    )
+  )
 }
