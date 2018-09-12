47f061fa6644d2a599f09b26b96f4db30ebdec2b
Martin Heidegger <martin.heidegger@gmail.com>
2018-09-13 00:23:33 +0900

    Added util to subtract a number | Long from number | Long

---
+++ ts/util/subtractNumber.ts
@@ -0,0 +1,20 @@
+import Long from 'long'
+
+function process (long: Long): number | Long {
+  if (long.lte(Number.MAX_SAFE_INTEGER)) {
+    return long.toNumber()
+  }
+  return long
+}
+
+export default function subtractNumber (a: number | Long, b: number | Long): number | Long {
+  if (typeof a === 'number') {
+    if (typeof b === 'number') {
+      if (a <= Number.MAX_SAFE_INTEGER && b <= Number.MAX_SAFE_INTEGER) {
+        return a - b
+      }
+    }
+    return process(new Long(a).subtract(b))
+  }
+  return process(a.subtract(b))
+}
