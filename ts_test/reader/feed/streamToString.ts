0fcae1df59c9dc141dacb9dd21890b331b06829d
Martin Heidegger <martin.heidegger@gmail.com>
2018-09-29 03:45:50 +0900

    Updated streamToString test util to also use IReadable

--- ts_test/reader/feed/streamToString.ts
+++ ts_test/reader/feed/streamToString.ts
@@ -1,7 +1,8 @@
-import { combine } from '../../../ts/reader/util/combine'
 import decodeUtf8 from 'decode-utf8'
-import { ReadableStream } from 'ts-stream'
+import { IReadable } from '../../../ts/api/IReadable'
+import { combine } from '../../../ts/reader/util/combine'
+import { getAll } from '../../../ts/util/getAll'
 
-export async function streamToString (stream: ReadableStream<Uint8Array>): Promise<string> {
-  return decodeUtf8(combine(await stream.toArray()))
+export async function streamToString (stream: IReadable<Uint8Array>): Promise<string> {
+  return decodeUtf8(combine(await getAll(stream)))
 }
