71baed44d4b1feebd1e5f1e7c65ffebf9c1ddb49
Martin Heidegger <martin.heidegger@gmail.com>
2018-09-29 03:44:12 +0900

    Added way to override abort default behaviour

--- ts/reader/streamConv.ts
+++ ts/reader/streamConv.ts
@@ -2,7 +2,15 @@ import { Stream as TsStream } from 'ts-stream'
 import { Readable, Writable, WritableOptions } from 'stream'
 import { mapSeries } from 'bluebird'
 
-export function streamConv<t> (from: Readable, byos: TsStream<t> = new TsStream<t>()): TsStream<t> {
+function destroyOnAbort (from: Readable, err: Error) {
+  from.destroy(err)
+}
+
+export function streamConv<t> (
+  from: Readable,
+  byos: TsStream<t> = new TsStream<t>(),
+  onAbort: (from: Readable, err: Error) => void = destroyOnAbort
+): TsStream<t> {
   from.pipe(new Writable({
     write: (chunk, encoding, callback: (error?: Error) => void) =>
       byos
@@ -23,6 +31,7 @@ export function streamConv<t> (from: Readable, byos: TsStream<t> = new TsStream<
         .then(() => callback())
         .catch(callback)
   }))
-  byos.aborted().then(_ => from.destroy())
+  byos.aborted()
+    .catch(err => onAbort(from, err))
   return byos
 }
