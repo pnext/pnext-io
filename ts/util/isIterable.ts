884ca18ff0adccbe3bda4630f320ebfaf4ca6fab
Martin Heidegger <martin.heidegger@gmail.com>
2018-10-02 00:55:51 +0900

    fixed isIterable as it didn't cover "Array" because it doesn't have Symbol.iterator accessible through "hasOwnProperty"

--- ts/util/isIterable.ts
+++ ts/util/isIterable.ts
@@ -1,4 +1,4 @@
 // Following same logic as isPromiseLike
 export function isIterable <T> (arg: T | Iterable<T> | undefined | null): arg is Iterable<T> {
-  return typeof arg === 'object' && arg.hasOwnProperty(Symbol.iterator)
+  return typeof arg === 'object' && typeof arg[Symbol.iterator] === 'function'
 }
