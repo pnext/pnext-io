f83ea3f15f1c112a7f33a339067485774c2839fb
Martin Heidegger <martin.heidegger@gmail.com>
2018-11-04 23:34:00 +0900

    Used ILocationFeed in Las API - 3

--- ts/format/las/header/readTree.ts
+++ ts/format/las/header/readTree.ts
@@ -1,6 +1,6 @@
+import { ILocationFeed } from '../../../reader/feed/ILocationFeed'
 import { fromHeader } from './fromHeader'
 import { readHeader } from './readHeader'
-import { IFeed } from '../../../reader/feed/IFeed'
 
 export async function readTree (feed: ILocationFeed, nodeLimit?: number) {
   return fromHeader(await readHeader(feed), location, nodeLimit)
