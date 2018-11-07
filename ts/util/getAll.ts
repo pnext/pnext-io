d9655fd9f6484948888058cf92184b7a9c2e7ac5
Martin Heidegger <martin.heidegger@gmail.com>
2018-11-07 23:31:54 +0900

    More code cleanup.

--- ts/util/getAll.ts
+++ ts/util/getAll.ts
@@ -4,7 +4,6 @@ import { createLazyPromise } from './createLazyPromise'
 export function getAll <T> (stream: IReadable<T>): Promise<T[]> {
   const p = createLazyPromise<T[]>()
   const list = []
-  let oneFound = false
   stream.forEach(
     item => { list.push(item) },
     (error?: Error) => {
